9cda708bb3418057e45eef8e70d42ce9e8ae30af
Mikhail Baranov <uasekus@gmail.com>
2022-02-03 00:30:34 +0300

    SafeSelectByShuffling signature update

--- modules/feature_selection.py
+++ modules/feature_selection.py
@@ -1,5 +1,7 @@
-from feature_engine.creation import CombineWithReferenceFeature
+import numpy as np
 
+from feature_engine.creation import CombineWithReferenceFeature
+from feature_engine.selection import SelectByShuffling, SelectBySingleFeaturePerformance
 
 class CombineWithReferenceFeature_adj():
     '''
@@ -24,3 +26,66 @@ class CombineWithReferenceFeature_adj():
     
     def transform(self, X):
         return(self.combinator.transform(X))        
+
+
+class SafeSelectByShuffling(SelectByShuffling):
+    """
+    A wrapper for SelectByShuffling from feature_engine.selection.
+    This class safely handles cases when all features are dropped by its ancestor.
+
+    Set min_features to determine a minimum number of features to be returned. If
+    there is less than min_features left after selection by shuffling, the rest is
+    chosen based on performance drifts.
+
+    Warning! This implementation assumes that greater values of the metric, which is
+    set in the ancestor's parameters, means better model performance (e.g. roc auc score).
+
+    The signature has all the arguments from SelectByShuffling to follow scikit-learn
+    estimators convention to avoid varargs. This is necessary to make the class a part
+    of sklearn.pipeline.Pipeline.
+    """
+
+    def __init__(
+        self,
+        estimator,
+        min_features=1,
+        scoring = "roc_auc",
+        cv=3,
+        threshold = None,
+        variables = None,
+        random_state = None,
+    ):
+        super().__init__(
+            estimator=estimator,
+            scoring=scoring,
+            cv=cv,
+            threshold=threshold,
+            variables=variables,
+            random_state=random_state
+            )
+        self.min_features = min_features
+
+    def transform(self, X):
+
+        n_features_left  = self.n_features_in_ - len(self.features_to_drop_)
+
+        if n_features_left >= self.min_features:
+            return super().transform(X)
+
+        else:
+            m = self.min_features
+            print((
+                f"Less than min_features = {m} are left, "
+                f"return {m} best feature{'' if m == 1 else 's'} by performance drift."
+                ))
+
+            features, drifts = zip(*self.performance_drifts_.items())                     # split a dictionary into keys and values
+            features = np.array(features)[np.argsort(drifts)[::-1]]                       # sort feature names in descending order of metric drifts
+            return X[features[:self.min_features]]                                        # return self.min_features features with the best values
+
+
+class SafeSelectBySingleFeaturePerformance(SelectBySingleFeaturePerformance):
+    """
+    # TODO
+    """
+    pass
